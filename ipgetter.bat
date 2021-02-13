e9fa80bd5990438985bfd2da0888ca729184f370
Zac Nolan <zac1i1nolan@gmail.com>
2021-02-13 11:12:47 +0000

    Add files via upload

---
+++ ipgetter.bat
@@ -0,0 +1,7 @@
+@echo off
+set ip_address_string="IPv4 Address"
+for /f "usebackq tokens=2 delims=:" %%f in (`ipconfig ^| findstr /c:%ip_address_string%`) do (
+	echo%%f >"%~dp0\ClientIP.txt"
+)
+
+
